595b304a1d1899f97cba984c67367a9fab664c71
AmitGurevich <69407198+AmitGurevich@users.noreply.github.com>
2024-02-02 12:53:57 +0200

    Update Main.cc

--- Main.cc
+++ Main.cc
@@ -1,4 +1,6 @@
 #include <Wire.h>
+#include <Pixy2.h>
+
 #define motor1Pin1 2
 #define motor1Pin2 4
 #define motor1EnablePin 3
@@ -9,16 +11,27 @@
 #define trigPin 9
 #define echoPin 8
 
-#define distanceThreshold 20
+#define distanceThreshold 10
 #define pixyI2CAddress 0x54
+#define MotorSpeed 50
 
-// Pixy camera variables
+// Pixy camera and global variables
 int pixyXCenter = 0;
 int pixyYCenter = 0;
 int pixyWidth = 0;
 int pixyHeight = 0;
+int Distance;
+bool BallDetected;
+bool GoalDetected;
+bool BallCatched;
+
+Pixy2 pixy;
 
 void setup() {
+
+  // initualizing pixy 
+  pixy.init();
+  
   // Configure motor pins as output
   pinMode(motor1Pin1, OUTPUT);
   pinMode(motor1Pin2, OUTPUT);
@@ -31,14 +44,7 @@ void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
-  // Configure Pixy camera
-  Wire.begin();
-  Wire.beginTransmission(pixyI2CAddress);
-  Wire.write(0x00);  // Set Pixy to color code mode
-  Wire.write(0x00);  // Set color code to red
-  Wire.endTransmission();
-
-  // Set initial motor direction
+  // Set initial motor direction(this is made for knowing the direction of the motors)
   digitalWrite(motor1Pin1, HIGH);
   digitalWrite(motor1Pin2, LOW);
   digitalWrite(motor2Pin1, HIGH);
@@ -49,66 +55,49 @@ void setup() {
 }
 
 void loop() {
-  long duration, distance;
+  // calculating distance
+  long duration, Distance;
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
-  distance = duration * 0.034 / 2;
+  Distance = duration * 0.034 / 2;
 
-  Serial.print("Distance: ");
-  Serial.println(distance);
-
-  // Read Pixy camera data
-  Wire.requestFrom(pixyI2CAddress, 14);
-  if (Wire.available() == 14) {
-    pixyXCenter = Wire.read() << 8 | Wire.read();
-    pixyYCenter = Wire.read() << 8 | Wire.read();
-    pixyWidth = Wire.read() << 8 | Wire.read();
-    pixyHeight = Wire.read() << 8 | Wire.read();
-  }
-
-  // Check if an object is detected
-  if (pixyWidth > 0 && pixyHeight > 0) {
-    // Object detected, move towards it
-    if (pixyXCenter < 160) {
-      // Object is to the left, turn left
-      moveLeft();
-    } else if (pixyXCenter > 320) {
-      // Object is to the right, turn right
-      moveRight();
-    } else {
-      // Object is centered, move forward
-      moveForward();
+  int Blocks = pixy.ccc.getBlocks();
+  if (Blocks > 0) {
+    for (int i = 0; i < Blocks; i++) {
+      if (pixy.ccc.blocks[i].m_signature == 1) { // don't forget to replace 1 with name of the color signature!!!
+        pixyXCenter = pixy.ccc.blocks[i].m_x;
+        pixyYCenter = pixy.ccc.blocks[i].m_y;
+        pixyWidth = pixy.ccc.blocks[i].m_width;
+        pixyHeight = pixy.ccc.blocks[i].m_height;
+        
+        BallDetected = true;
+        Follow();
+      }
     }
-  } else {
-    // No object detected, move forward
-    moveForward();
+  }
+  else{
+    SearchBall();
   }
 
-  // Check if the robot is too close to an obstacle
-  if (distance <= distanceThreshold || distance > 2000) {
-    // Obstacle detected, move backward
-    moveBackward();
-  } else if (20 <= distance && distance <= 30){
-    // Obstacle detected, stop motors
-    stopMotors();
-  }
 }
 
-void moveForward() {
+void MoveForward() {
   digitalWrite(motor1Pin1, HIGH);
   digitalWrite(motor1Pin2, LOW);
   digitalWrite(motor2Pin1, HIGH);
   digitalWrite(motor2Pin2, LOW);
 
-  analogWrite(motor1EnablePin, 255);
-  analogWrite(motor2EnablePin, 255);
+  analogWrite(motor1EnablePin, MotorSpeed);
+  analogWrite(motor2EnablePin, MotorSpeed);
+
+  Serial.println("MoveForward");
 }
 
-void stopMotors() {
+void StopMotors() {
   digitalWrite(motor1Pin1, LOW);
   digitalWrite(motor1Pin2, LOW);
   digitalWrite(motor2Pin1, LOW);
@@ -116,35 +105,70 @@ void stopMotors() {
 
   analogWrite(motor1EnablePin, 0);
   analogWrite(motor2EnablePin, 0);
+
+  Serial.println("stopmotors");
 }
 
-void moveBackward(){
+void MoveBackward(){
   digitalWrite(motor1Pin1, LOW);
   digitalWrite(motor1Pin2, HIGH);
   digitalWrite(motor2Pin1, LOW);
   digitalWrite(motor2Pin2, HIGH);
 
   // Set motor speeds to zero
-  analogWrite(motor1EnablePin, 255);
-  analogWrite(motor2EnablePin, 255);
+  analogWrite(motor1EnablePin, MotorSpeed);
+  analogWrite(motor2EnablePin, MotorSpeed);
+
+  Serial.println("Movebackwards");
 }
 
-void moveLeft() {
+void MoveLeft() {
   digitalWrite(motor1Pin1, LOW);
   digitalWrite(motor1Pin2, HIGH);
   digitalWrite(motor2Pin1, HIGH);
   digitalWrite(motor2Pin2, LOW);
 
-  analogWrite(motor1EnablePin, 255);
-  analogWrite(motor2EnablePin, 255);
+  analogWrite(motor1EnablePin, MotorSpeed);
+  analogWrite(motor2EnablePin, MotorSpeed);
+
+  Serial.println("MoveLeft");
 }
 
-void moveRight() {
+void MoveRight() {
   digitalWrite(motor1Pin1, HIGH);
   digitalWrite(motor1Pin2, LOW);
   digitalWrite(motor2Pin1, LOW);
   digitalWrite(motor2Pin2, HIGH);
 
-  analogWrite(motor1EnablePin, 255);
-  analogWrite(motor2EnablePin, 255);
+  analogWrite(motor1EnablePin, MotorSpeed);
+  analogWrite(motor2EnablePin, MotorSpeed);
+
+  Serial.println("MoveRight");
+}
+
+void SearchBall(){
+  MoveLeft();
+}
+void SearchGoal(){
+  Follow();
+}
+
+void Follow() {
+  // Check if an object is detected
+  if (pixyWidth > 0 && pixyHeight > 0) {
+    // Object detected, move towards it
+    if (pixyXCenter <= 50) {
+      // Object is to the left, turn left
+      MoveLeft();
+    } else if (pixyXCenter > 250) {
+      // Object is to the right, turn right
+      MoveRight();
+    } else {
+      //object is centered,go get it!
+      MoveForward();
+    }
+  } else {
+    // No object detected, move forward
+    MoveForward();
+  }
 }
